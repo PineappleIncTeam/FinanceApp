606306cccf032a7365ce11f93d0c676d46679d69
masian4eg <masian28@gmail.com>
2022-10-25 22:33:10 +0700

    2510 фикс моделей, добавил сериализаторы

---
+++ api/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from api.models import User, Categories, OutcomeCash, IncomeCash, MoneyBox
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'date_joined']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categories
+        fields = ['categoryName', ]
+
+
+class OutcomeCashSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OutcomeCash
+        fields = ['reg_sum', 'var_sum', 'categories', 'date', 'user']
+
+
+class IncomeCashSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IncomeCash
+        fields = ['reg_sum', 'var_sum', 'categories', 'date', 'user']
+
+
+class MoneyBoxSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MoneyBox
+        fields = ['box_name', 'box_sum', 'user']
