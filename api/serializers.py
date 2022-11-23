6e6b571822eb3ecd8378bde3b50af9612bc9799b
rogiGoodwill <dolgow313@gmai.com>
2022-11-23 15:09:27 +0400

    Изменил формат даты на вывод

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@ from django.db.models.aggregates import Sum
 from rest_framework import serializers
 from .models import User, Categories, OutcomeCash, IncomeCash, MoneyBox
 from django.http import JsonResponse
-
+from datetime import datetime, date
 
 
 # class UserSerializer(serializers.ModelSerializer):
@@ -36,7 +36,7 @@ class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Categories
-        fields = ['categoryName', 'category_id', 'category_type', 'user_id' ]
+        fields = ['categoryName', 'category_id', 'category_type', 'user_id']
 
 
 class OutcomeCashSerializer(serializers.ModelSerializer):
@@ -45,63 +45,69 @@ class OutcomeCashSerializer(serializers.ModelSerializer):
         fields = ['constant_sum', 'once_sum', 'categories', 'date', 'user']
 
 
-# class IncomeCashSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = IncomeCash
-#         fields = ['reg_sum', 'var_sum', 'categories', 'date', 'user']
-
 
 class MoneyBoxSerializer(serializers.ModelSerializer):
     class Meta:
         model = MoneyBox
         fields = ['box_name', 'box_sum', 'user']
 
+
 class IncomeCashSerializer(serializers.ModelSerializer):
     user = serializers.CharField(required=False)
     category_id = serializers.IntegerField(source='categories_id')
     categoryName = serializers.CharField(source='categories.categoryName', required=False)
     category_type = serializers.CharField(source='categories.category_type', required=False)
-    # constant_sum = serializers.DecimalField(max_digits=19, decimal_places=2 ,required=False, default=0)
-    # once_sum = serializers.DecimalField(max_digits=19, decimal_places=2, required=False, default=0)
     sum = serializers.DecimalField(max_digits=19, decimal_places=2, required=False, default=0)
-    date = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', required=False)
+    # date = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S, %a', required=False)
+    date = serializers.SerializerMethodField(required=False)
+
     class Meta:
         model = IncomeCash
-        # fields = ('user', 'category_id', 'categoryName', 'category_type', 'constant_sum', 'once_sum', 'date')
         fields = ('user', 'category_id', 'categoryName', 'category_type', 'sum', 'date')
+
     def create(self, validated_data):
         user_id = self.context.get('request').user.pk
         category_id = validated_data.__getitem__('categories_id')
-        # category_type = Categories.objects.get(id=category_id).category_type
         sum = self.validated_data.__getitem__('sum')
 
         try:
-            Categories.objects.get(user_id=user_id, id = category_id)
+            Categories.objects.get(user_id=user_id, id=category_id)
 
             incomecash = IncomeCash.objects.create(
                 user_id=user_id,
                 categories_id=category_id,
-                sum=sum,)
+                sum=sum, )
             return incomecash
         except:
             raise ValueError(f"У пользователя с id {user_id} нет категории с id {category_id}")
+
+    def get_date(self, validated_data):
+        days = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"]
+        monthes = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября",
+                   "Ноября", "Декабря"]
+        today = validated_data.date
+        num_week_day = datetime.weekday(today)
+        num_month = int(datetime.strftime(today, '%m')) - 1
+        return datetime.strftime(today, f'%d {monthes[num_month]} %Y, {days[num_week_day]}')
+
 
 class SumIncomeCashSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField(source='user')
     constant_sum = serializers.SerializerMethodField()
     once_sum = serializers.SerializerMethodField()
 
-    def get_constant_sum(self,validated_data):
+    def get_constant_sum(self, validated_data):
         user_id = self.context.get('request').user.pk
-        constant_sum = IncomeCash.objects.filter(user_id=user_id, categories__category_type='constant').aggregate(Sum('sum')).get('sum__sum', 0.00)
+        constant_sum = IncomeCash.objects.filter(user_id=user_id, categories__category_type='constant').aggregate(
+            Sum('sum')).get('sum__sum', 0.00)
         return constant_sum
 
-    def get_once_sum(self,validated_data):
+    def get_once_sum(self, validated_data):
         user_id = self.context.get('request').user.pk
-        once_sum = IncomeCash.objects.filter(user_id=user_id, categories__category_type='once').aggregate(Sum('sum')).get('sum__sum',0.00)
+        once_sum = IncomeCash.objects.filter(user_id=user_id, categories__category_type='once').aggregate(
+            Sum('sum')).get('sum__sum', 0.00)
         return once_sum
 
     class Meta:
         model = IncomeCash
-        fields = ('user_id','constant_sum', 'once_sum')
-
+        fields = ('user_id', 'constant_sum', 'once_sum')
