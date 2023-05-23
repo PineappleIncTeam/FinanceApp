cc248e4e45f6ae0919350a9ce5d4d327e858e998
alexander_potapkov <xanben@mail.ru>
2023-05-23 09:17:56 +0300

    add last 5 money box

--- api/signals.py
+++ api/signals.py
@@ -1,13 +1,10 @@
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
-
-from api.models import MoneyBox, OutcomeCash
+from .models import MoneyBox, OutcomeCash
 
 
 @receiver(pre_delete, sender=MoneyBox)
-# Сигнал pre_delete удаляет все записи, связанные с MoneyBox из OutcomeCash,
-# при условии box_sum < box_target
-
 def delete_related_outcome_cash(sender, instance, **kwargs):
-    if instance.box_sum < instance.box_target:
-        OutcomeCash.objects.filter(user=instance.user, categories_id=instance.categories_id).delete()
+    outcome_cash = OutcomeCash.objects.filter(user=instance.user, categories=instance.categories)
+    if outcome_cash:
+        outcome_cash.delete()
