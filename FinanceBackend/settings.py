5620e61f43ca6a00348d0962f4b961c82b603740
masian4eg <masian28@gmail.com>
2023-10-24 23:52:05 +0700

    фикс статики

--- FinanceBackend/settings.py
+++ FinanceBackend/settings.py
@@ -87,12 +87,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/freenance/static/'
-STATICFILES_DIRS = [
-    # BASE_DIR / "static",
-    "/var/www/freenance/static/",
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
