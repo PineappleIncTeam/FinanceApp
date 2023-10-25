f2ad20d82485ccc25c256f1267ee65c54feb27d9
masian4eg <masian28@gmail.com>
2023-10-25 12:55:04 +0700

    фикс статики

--- FinanceBackend/settings.py
+++ FinanceBackend/settings.py
@@ -88,7 +88,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = './static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
--- FinanceBackend/wsgi.py
+++ FinanceBackend/wsgi.py
@@ -9,13 +9,8 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 
-from whitenoise import WhiteNoise
-
 from django.core.wsgi import get_wsgi_application
-
-from FinanceBackend.settings import BASE_DIR
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FinanceBackend.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'static'))
