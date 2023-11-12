cc668f6e4d166d65f2167ed16344abe84787787d
masian4eg <masian28@gmail.com>
2023-11-12 14:48:06 +0700

    fix swagger

--- FinanceBackend/settings.py
+++ FinanceBackend/settings.py
@@ -88,7 +88,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 STATIC_ROOT = 'staticfiles'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
