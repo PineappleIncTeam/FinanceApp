ee8b061a403c273637d63753ffd08d2a97f3145f
kiril-lysenko <103587102+kiril-lysenko@users.noreply.github.com>
2024-01-15 12:22:55 +0300

    Update spectacular.py

--- FinanceBackend/spectacular.py
+++ FinanceBackend/spectacular.py
@@ -3,6 +3,6 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerSplitView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
