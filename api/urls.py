e611f1156ed79df9d8adc714ff6bc09b39225cf1
kiril-lysenko <103587102+kiril-lysenko@users.noreply.github.com>
2024-01-15 11:35:18 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,6 @@ from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from django.conf import settings
 from django.conf.urls.static import static
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 
 from .views import (
     GetCreateCategoryAPIView,
@@ -41,18 +39,6 @@ from .views import (
 )
 
 router = DefaultRouter()
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
 urlpatterns = [
     path(
         'categories/',
@@ -236,8 +222,6 @@ urlpatterns = [
     # path('api/', include(router.urls)),
     # path('api/registration/', CreateUser.as_view(), name='create-user'),
     # path('api/drf-auth/', include('rest_framework.urls'))
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
