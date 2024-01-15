e85a4925c10dc04509b37846f74fff1091503c0c
kiril-lysenko <103587102+kiril-lysenko@users.noreply.github.com>
2024-01-15 14:48:37 +0300

    Update settings.py

--- FinanceBackend/settings.py
+++ FinanceBackend/settings.py
@@ -118,11 +118,15 @@ SPECTACULAR_SETTINGS = {
     'TITLE': 'Freenance API',
     'DESCRIPTION': 'ololo pishch pishch',
     'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': False,
+    'SERVE_INCLUDE_SCHEMA': True,
     'SERVE_PUBLIC': True,
     'SWAGGER_UI_DIST': 'SIDECAR',
     'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
     'REDOC_DIST': 'SIDECAR',
+    'SERVERS': [
+    {'url': 'https://dev.freenance.store/api/', 'description': 'Основной сервер'},
+    
+]
     'SWAGGER_UI_FAVICON_HREF': 'https://cdn.jsdelivr.net/npm/swagger-ui-dist@latest/favicon-32x32.png',
 }
 CSP_DEFAULT_SRC = ("'self'", "'unsafe-inline'")
