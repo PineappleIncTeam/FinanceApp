39bc1dd746d13e5195cc453aa6f375a559fa5db1
Valery Tsischanka <houlett95@gmail.com>
2024-11-03 21:09:10 +0300

    fix post method in operation.py

--- api/views/operation.py
+++ api/views/operation.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from rest_framework import filters
+from rest_framework import filters, status
 from rest_framework.generics import (ListCreateAPIView,
                                      RetrieveUpdateDestroyAPIView)
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from api.models import Operation
 from api.serializers import OperationSerializer
@@ -26,6 +27,14 @@ class OperationListCreateAPI(ListCreateAPIView):
 
     def get_queryset(self) -> QuerySet[Operation]:
         return Operation.objects.filter(user=self.request.user)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(user=self.request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def filter_queryset(self, queryset):
         operation_type = self.request.query_params.get("type", None)
