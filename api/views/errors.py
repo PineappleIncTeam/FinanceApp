3996bc0038982a85e99d2f008c04d6fcffb81476
Ekaterina Itchenko <ekaterina.savickaya.1989@gmail.com>
2024-11-05 21:17:11 +0300

    Fix status_code in CategoryWithOperationsError

--- api/views/errors.py
+++ api/views/errors.py
@@ -1,7 +1,8 @@
 from rest_framework.exceptions import APIException
+from rest_framework.status import HTTP_400_BAD_REQUEST
 
 
 class CategoryWithOperationsError(APIException):
-    status_code = 400
+    status_code = HTTP_400_BAD_REQUEST
     default_detail = "Can not delete category with existing operations."
     default_code = "operations_exists"
