cc2db58ba9880e48190d2c80f2f467bd140d3a2b
Ekaterina Itchenko <ekaterina.savickaya.1989@gmail.com>
2024-11-05 21:22:18 +0300

    Fix status_code in CategoryExistsError

--- api/serializers/errors.py
+++ api/serializers/errors.py
@@ -1,7 +1,8 @@
 from rest_framework.exceptions import APIException
+from rest_framework.status import HTTP_400_BAD_REQUEST
 
 
 class CategoryExistsError(APIException):
-    status_code = 400
+    status_code = HTTP_400_BAD_REQUEST
     default_detail = "The category already exists."
     default_code = "category_exists"
