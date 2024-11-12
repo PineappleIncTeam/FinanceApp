1396d953fce0a2e5716a126349ba7dc1c2d4524c
TannedRose <smirnoffmir2010@gmail.com>
2024-11-12 21:58:59 +0300

    ...

--- api/migrations/0019_add_all_users_profile.py
+++ api/migrations/0019_add_all_users_profile.py
@@ -4,10 +4,8 @@ from django.db import migrations, utils
 def load_data(apps, schema_editor):
     profile = apps.get_model('api', 'Profile')
     user = apps.get_model('api', 'User')
-    country = apps.get_model('api', 'Country')
 
     users = user.objects.all()
-    c = country.objects.get(code='RU')
     for us in users:
         profile.objects.get_or_create(user=us)
 
