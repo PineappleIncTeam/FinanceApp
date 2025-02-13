0cc2343fd7207097dcf629730213cb45af776554
TannedRose <smirnoffmir2010@gmail.com>
2025-02-13 22:50:59 +0300

    add_query_operation

--- api/views/operation.py
+++ api/views/operation.py
@@ -111,7 +111,7 @@ class OperationListCreateAPI(GenericAPIView):
                 target.save()
             serializer.save(user=self.request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)\
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class OperationRetrieveUpdateDestroyAPI(GenericAPIView):
