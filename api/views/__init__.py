ba58129bd121f3f16e98efa833d3031435a11902
TannedRose <smirnoffmir2010@gmail.com>
2025-08-06 12:33:28 +0300

    jwt auth

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,4 +1,5 @@
-from .categories import CategoriesListCreateAPI, CategoryUpdateDestroyAPI
+from .categories import CategoriesListCreateAPI, CategoryUpdateDestroyAPI, CategoryGetAPI
+from .cheque import ChequeView
 from .countries import CountriesApiView
 from .generate_token import CustomTokenCreateAPI
 from .login import LoginView, TokenRefreshView
@@ -34,6 +35,8 @@ __all__ = [
     "CustomLogoutView",
     "OperationPDFView",
     "OperationXLSView",
+    "ChequeView",
+    "CategoryGetAPI",
     "LoginView",
     "TokenRefreshView"
 ]
